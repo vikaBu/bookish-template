b4d55f53c05693459daa1775e2049bd162834dc7
Mike Walker <michael.walker@softwire.com>
2020-08-14 02:30:15 +0100

    Add Nunjucks

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 ﻿import "dotenv/config";
 import express from "express";
+import nunjucks from "nunjucks";
 
 const app = express();
 const port = process.env['PORT'] || 3000;
 
+const PATH_TO_TEMPLATES = "./src/templates/";
+nunjucks.configure(PATH_TO_TEMPLATES, { 
+    autoescape: true,
+    express: app
+});
+
 app.get("/", (req, res) => {
-    res.send(process.env['SECRET_VALUE']);
+    const model = {
+        message: process.env['SECRET_VALUE']
+    }
+    res.render('index.html', model);
 });
 
 app.listen(port, () => {
