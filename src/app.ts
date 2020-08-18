e570a1920e97ed7684fc93dce6a4f0471956c6ce
vikaBu <vikabuklajevafoto@gmail.com>
2020-08-18 13:53:36 +0100

    tuesday commit

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { title } from "process";
 import {book_list} from "./querySelector"
 import { request } from "http";
 import {new_book} from "./querySelector"
+import {book_removal} from "./querySelector"
+import {delete_book} from "./querySelector"
 
 
 
@@ -56,22 +58,30 @@ app.get("/books/addbook", (request, response) => {
 })
  
 app.post("/books/addbook", async (request, response) =>{
-    const book = request.body
-    await new_book(book)
+    const book = request.body;
+    await new_book(book);
     response.send("Success! check out your new entry in : 'http://localhost:3000/book_list' ")
 })
 
 app.get("/book/:name", async (request, response) => {
-    const name = request.params.name
-    const sqlResult = await book_name(name)
-    response.json(sqlResult)
+    const name = request.params.name;
+    const sqlResult = await book_name(name);
+    response.json(sqlResult);
 })
 
-// app.get("/book/:list", async (request, response) => {
-//     const name = request.params.body
-//     const sqlResult = await book_name(name)
-//     response.json(sqlResult)
-// })
+app.get("/books/remove",(request, response) => {
+    response.render('removalPage.html');
+})
+
+app.post("/books/remove", async (request, response) =>{
+    const removal = request.body;
+    await delete_book(removal.id);
+    await book_removal(removal.id);
+    
+    response.send('book removed');
+})
+
+
 
 
 app.listen(port, () => {
--- src/querySelector.ts
+++ src/querySelector.ts
@@ -38,3 +38,21 @@ export const new_book = (book : Book ) => {
     return client.insert({title: book.title , author: book.author, genre: book.genre, release: book.release }).into("books")
     
 }
+
+export const book_removal = (id : number) => {
+    console.log(id)
+    return client('books')
+    .delete()
+    .where ('id', id)
+    
+}
+
+export const delete_book = (id : number) =>{
+    return client ('copies_of_books')
+    .delete()
+    .where ('book_id', id)
+}
+
+export const update_book = (book: Book) => {
+
+}
